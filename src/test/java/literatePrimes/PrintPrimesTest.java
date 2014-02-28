52902de90c55fdf88714c1f202ac7ecbc8136458
James Murphy <jamurphy84@hotmail.com>
2014-02-28 09:26:22 +0000

    Adding files for initial setup of dojo

---
+++ src/test/java/literatePrimes/PrintPrimesTest.java
@@ -0,0 +1,13 @@
+package literatePrimes;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+public class PrintPrimesTest {
+
+    @Test
+    public void firstTest() throws Exception {
+        fail("Please implement your first test");
+    }
+}
