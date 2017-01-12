4b81f7bf9f32cbd6bcbb86ef94a6db2ab7cefd82
murphyj <jmurphy84@googlemail.com>
2017-01-12 20:07:02 +0000

    Extracted generateOutput to a class PrimePrinter

---
+++ src/main/java/literatePrimes/PrimePrinter.java
@@ -0,0 +1,29 @@
+package literatePrimes;
+
+public class PrimePrinter {
+
+    public static String generateOutput(PrintConfiguration configuration, int[] primes, StringBuffer primesOutput) {
+        int pageNumber;
+        int pageOffset;
+        int rowOffset;
+        int currentColumnNum;
+        pageNumber = 1;
+        pageOffset = 1;
+
+        while (pageOffset <= configuration.getMaxNumPrimes()) {
+            primesOutput.append("The First " + configuration.getMaxNumPrimes() + " Prime Numbers --- Page " + pageNumber);
+            primesOutput.append("\n");
+            for (rowOffset = pageOffset; rowOffset < pageOffset + configuration.getRowsPerPage(); rowOffset++) {
+                for (currentColumnNum = 0; currentColumnNum < configuration.getColsPerPage(); currentColumnNum++)
+                    if (rowOffset + currentColumnNum * configuration.getRowsPerPage() <= configuration.getMaxNumPrimes()) {
+                        String offset = String.format("%10d", primes[rowOffset + currentColumnNum * configuration.getRowsPerPage()]);
+                        primesOutput.append(offset);
+                    }
+                primesOutput.append("\n");
+            }
+            pageNumber = pageNumber + 1;
+            pageOffset = pageOffset + configuration.getRowsPerPage() * configuration.getColsPerPage();
+        }
+        return primesOutput.toString();
+    }
+}
--- src/main/java/literatePrimes/PrintPrimes.java
+++ src/main/java/literatePrimes/PrintPrimes.java
@@ -52,31 +52,6 @@ public class PrintPrimes {
             primes[currentPrimeNumber] = nextNumber;
         }
 
-        return generateOutput(configuration, primes, primesOutput);
-    }
-
-    private static String generateOutput(PrintConfiguration configuration, int[] primes, StringBuffer primesOutput) {
-        int pageNumber;
-        int pageOffset;
-        int rowOffset;
-        int currentColumnNum;
-        pageNumber = 1;
-        pageOffset = 1;
-
-        while (pageOffset <= configuration.getMaxNumPrimes()) {
-            primesOutput.append("The First " + configuration.getMaxNumPrimes() + " Prime Numbers --- Page " + pageNumber);
-            primesOutput.append("\n");
-            for (rowOffset = pageOffset; rowOffset < pageOffset + configuration.getRowsPerPage(); rowOffset++) {
-                for (currentColumnNum = 0; currentColumnNum < configuration.getColsPerPage(); currentColumnNum++)
-                    if (rowOffset + currentColumnNum * configuration.getRowsPerPage() <= configuration.getMaxNumPrimes()) {
-                        String offset = String.format("%10d", primes[rowOffset + currentColumnNum * configuration.getRowsPerPage()]);
-                        primesOutput.append(offset);
-                    }
-                primesOutput.append("\n");
-            }
-            pageNumber = pageNumber + 1;
-            pageOffset = pageOffset + configuration.getRowsPerPage() * configuration.getColsPerPage();
-        }
-        return primesOutput.toString();
+        return PrimePrinter.generateOutput(configuration, primes, primesOutput);
     }
 }
